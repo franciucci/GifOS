831c19c0aba0dbbccf800778ba24f5844202e3f7
Franco Ciucci <ciuccifj@gmail.com>
2021-01-05 13:34:25 -0300

    Agrego funcionalidad de autocompletar sugerencias en search bar

--- script/main.js
+++ script/main.js
@@ -1,4 +1,6 @@
-/* Burger menu */
+/* ********* NAVBAR******** */
+
+// Burger menu
 const burgerMenu = document.getElementById("burger");
 const menuLinks = document.getElementById("navlinks");
 const borderLinks = document.getElementsByClassName("links-border");
@@ -7,7 +9,7 @@ burgerMenu.addEventListener("click", showMenu);
 // Function that shows the menu on mobile version
 function showMenu() {
     menuLinks.classList.toggle("menu-hide");
-    for (i = 0; i < borderLinks.length; i++) {
+    for (let i = 0; i < borderLinks.length; i++) {
         borderLinks[i].classList.toggle("borders-actives");
     }
     changeBurgerMenu(); 
@@ -20,3 +22,115 @@ function changeBurgerMenu() {
         burgerMenu.src = "https://gif-os.netlify.app/assets/mobile/burger.svg"
     }
 }
+
+/* ********TRENDINGS TAGS********* */
+
+// Request to Giphy API to show trending tags
+async function getTrendingTags() {
+    const url = `${trendingTagsEndpoint}?api_key=${apiKey}`;
+     await fetch(url).then(response => response.json())
+        .then(json => {
+            const trendingTagsArray = json.data.slice(0, 5);
+            const trendingTagsCapitalize = trendingTagsArray.map((word) => {
+                return word[0].toUpperCase() + word.substring(1);
+            }).join(",")
+            const newText = trendingTagsCapitalize.toString().split(",").join(", ");
+            $trendingSearchTags.textContent = newText;
+        })
+        .catch(err => console.log(err.message));
+}
+
+getTrendingTags();
+
+/* ******** SEARCH BAR ********** */
+
+$searchInput.addEventListener("focus", setActiveSearch);
+$searchInput.addEventListener("input", showSearchSuggestions);
+$searchInput.addEventListener("input", cleanSearchSuggestions);
+$searchInput.addEventListener("focusout", setInactiveSearch);
+
+/* Sets active search bar */
+function setActiveSearch() {
+    $searchSuggestions.classList.remove("hidden");
+    $searchBar.classList.add("searchActive");
+    $searchIcon.classList.add("hidden");
+    $closeSearchIcon.classList.remove("hidden");
+    $searchIconActive.classList.remove("hidden");
+}
+
+/* Sets inactive search bar */
+function setInactiveSearch() {
+    $searchSuggestions.classList.add("hidden");
+    $searchBar.classList.remove("searchActive");
+    $searchIcon.classList.remove("hidden");
+    $closeSearchIcon.classList.add("hidden");
+    $searchIconActive.classList.add("hidden");
+}
+
+/* Resets list of search suggestions */
+function cleanSearchSuggestions() {
+    $searchSuggestions.classList.add("hidden");
+    $searchSuggestionList.innerHTML = "";
+}
+//Request to Giphy API to autocomplete search suggestions
+async function showSearchSuggestions(e) {
+    cleanSearchSuggestions();
+    let inputValue = e.target.value;
+    const url = `${searchAutocompleteEndpoint}?api_key=${apiKey}&q=${inputValue}&limit=5`;
+    await fetch(url)
+        .then(response => response.json())
+        .then(result => {
+            for (let i=0; i < result.data.length; i++) {
+                const li = document.createElement("li");
+                $searchSuggestionList.appendChild(li);
+                li.innerHTML = `<img
+                src="assets/mobile/icon-search.svg"
+                alt="search icon"
+                id="searchList-icon"
+                onload="SVGInject(this)"
+              /> ${result.data[i].name}`
+            }
+        })
+        .catch(err => console.log(err.message))
+        setActiveSearch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Request to Giphy API to show trending gifs
+/* const url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=20&rating=pg`;
+
+fetch(url).then(response => response.json())
+    .then(json => {
+        let carousel = document.getElementById("track");
+        for (let i=0; i < json.data.length; i++) {
+            let slick = document.createElement("div");
+            slick.setAttribute("class", "slick");
+            carousel.appendChild(slick);
+            let slickGif = document.createElement("img");
+            slickGif.setAttribute("src", json.data[i].images.fixed_height.url);
+            slickGif.setAttribute("alt", json.data[i].title);
+            slick.appendChild(slickGif);
+        }    
+    })
+    .catch(err => console.log(err.message)) */
---
+++ script/variables.js
@@ -0,0 +1,28 @@
+/* ********* API ********** */
+
+const apiKey = "nENWhpLbxMys5qAUYSJiwgsHsdfA0cL4";
+
+// Endpoint for trendings
+const trendingEndpoint = "https://api.giphy.com/v1/gifs/trending";
+
+// Endpoint for searchs
+const searchEndpoint = "https://api.giphy.com/v1/gifs/search";
+
+// Endpoint to autocomplete searchs
+const searchAutocompleteEndpoint = "https://api.giphy.com/v1/gifs/search/tags";
+
+// Endpoint for trending tags
+const trendingTagsEndpoint = "https://api.giphy.com/v1/trending/searches";
+
+/* ******** DOM ITEMS ********* */
+
+const $trendingSearchTags = document.getElementById("trending-searchs__text");
+
+const $searchBar = document.getElementById("search-bar");
+const $searchInput = document.getElementById("search-input");
+const $searchSuggestions = document.getElementById("suggestion");
+const $searchSuggestionList = document.getElementById("suggestion-list");
+const $searchIconActive = document.getElementById("search-icon-active");
+const $searchListIcon = document.getElementById("searchList-icon");
+const $searchIcon = document.getElementById("search-icon");
+const $closeSearchIcon = document.getElementById("close-search-icon");
